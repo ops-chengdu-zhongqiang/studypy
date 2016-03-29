50b83ad72d53fd4800abe81e22d312305505a414
zhongqiang <zhongqiang@zhongqiangdeMacBook-Air.local>
2016-03-29 14:39:59 +0800

    add first_python.note

---
+++ py.py
@@ -0,0 +1,15 @@
+git clone https://github.com/ttxsgoto/courses
+
+git commit
+git add
+git put
+
+
+pip list ipython
+pip uninstall 
+cd /Users/zhongqiang/IDC/demo_app
+pip install -r requirements.txt 
+
+oh-my-zsh
+
+virtualenv  »·¾³ÇÐ·Ö
