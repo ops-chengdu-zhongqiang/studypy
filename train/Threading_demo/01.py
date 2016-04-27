d38cc78ae7cd4ecc72d6ea4402510a2665cc1dc8
ops-chengdu-zhongqiang <359450323@qq.com>
2016-04-27 18:19:34 +0800

    New: threading note

---
+++ train/Threading_demo/01.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+Created on 2016年4月25日
+
+@author: zhongqiang
+
+Describe: 多线程的实现方式和相关的方法
+'''
+
+from threading import Thread
+from time import sleep,ctime,sleep
+
+def myfunction(arg):
+    for item in range(10):
+        print item
+        sleep(1)
+
+
+print "before"
+t1 = Thread(target=myfunction,args=('hello',))
+print t1.getName() #Thread-1
+print t1.isDaemon() #默认为false
+#t1.setDaemon(True)  #设置为setDaemon模式
+
+t1.start()
+t1.join(timeout=5)
+
+print "after"
+print "Over"
+
+sleep(5)        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
